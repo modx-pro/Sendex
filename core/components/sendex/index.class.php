988146a6938bae7225b0432dd031b0dfc42ea7ce
Bezumkin <bezumkin@yandex.ru>
2013-11-16 15:22:51 +0700

    Improved load of class

--- core/components/sendex/index.class.php
+++ core/components/sendex/index.class.php
@@ -1,7 +1,5 @@
 <?php
 
-require_once dirname(__FILE__) . '/model/sendex/sendex.class.php';
-
 /**
  * Class SendexMainController
  */
@@ -14,6 +12,9 @@ abstract class SendexMainController extends modExtraManagerController {
 	 * @return void
 	 */
 	public function initialize() {
+		$corePath = $this->modx->getOption('sendex_core_path', null, $this->modx->getOption('core_path') . 'components/sendex/');
+		require_once $corePath . 'model/sendex/sendex.class.php';
+
 		$this->Sendex = new Sendex($this->modx);
 
 		$this->modx->regClientCSS($this->Sendex->config['cssUrl'] . 'mgr/main.css');
