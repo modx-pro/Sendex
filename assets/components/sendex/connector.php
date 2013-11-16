8863b2c6960c304464a9c24b6ba13db03f8aeac2
Bezumkin <bezumkin@yandex.ru>
2013-11-16 15:20:52 +0700

    Dev connector with error reporting

--- assets/components/sendex/connector.php
+++ assets/components/sendex/connector.php
@@ -1,6 +1,16 @@
 <?php
+// For debug
+ini_set('display_errors', 1);
+ini_set('error_reporting', -1);
 
-require_once dirname(dirname(dirname(dirname(__FILE__)))) . '/config.core.php';
+// Load MODX config
+if (file_exists(dirname(dirname(dirname(dirname(__FILE__)))) . '/config.core.php')) {
+	require_once dirname(dirname(dirname(dirname(__FILE__)))) . '/config.core.php';
+}
+else {
+	require_once dirname(dirname(dirname(dirname(dirname(__FILE__))))) . '/config.core.php';
+}
+
 require_once MODX_CORE_PATH . 'config/' . MODX_CONFIG_KEY . '.inc.php';
 require_once MODX_CONNECTORS_PATH . 'index.php';
 
