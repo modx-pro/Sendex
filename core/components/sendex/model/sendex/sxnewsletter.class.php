4d154cda6e514354fcb22d9d2b347d6303ae464e
Bezumkin <bezumkin@yandex.ru>
2013-12-12 16:12:50 +0700

    Subscription methods

--- core/components/sendex/model/sendex/sxnewsletter.class.php
+++ core/components/sendex/model/sendex/sxnewsletter.class.php
@@ -86,7 +86,6 @@ class sxNewsletter extends xPDOSimpleObject {
 			$queue->save();
 		}
 
-
 		return true;
 	}
 
@@ -97,7 +96,7 @@ class sxNewsletter extends xPDOSimpleObject {
 	 * @param int $user_id
 	 * @param string $email
 	 *
-	 * @return bool
+	 * @return int
 	 */
 	public function isSubscribed($user_id = 0, $email = '') {
 		$q = $this->xpdo->newQuery('sxSubscriber', array('newsletter_id' => $this->get('id')));
@@ -109,7 +108,144 @@ class sxNewsletter extends xPDOSimpleObject {
 			$q->where(array('email' => $email));
 		}
 
-		return (bool) $this->xpdo->getCount('sxSubscriber', $q);
+		/** @var sxSubscriber $subscriber */
+		if ($subscriber = $this->xpdo->getObject('sxSubscriber', $q)) {
+			return $subscriber->id;
+		}
+		else {
+			return 0;
+		}
+	}
+
+
+	/**
+	 * Method for send subscription link
+	 *
+	 * @param string $email
+	 * @param int $user_id
+	 * @param int $linkTTL
+	 *
+	 * @return bool|string
+	 */
+	public function checkEmail($email = '', $user_id = 0, $linkTTL = 1800) {
+		if (empty($email) && $profile = $this->xpdo->getObject('modUserProfile', array('internalKey' => $user_id))) {
+			$email = $profile->get('email');
+		}
+
+		if (empty($email) || !preg_match('/.+@.+\..+/i', $email)) {
+			return false;
+		}
+		elseif ($this->isSubscribed($user_id, $email)) {
+			return true;
+		}
+
+		$hash = sha1(uniqid(sha1($email), true));
+
+		/** @var modRegistry $registry */
+		$registry = $this->xpdo->getService('registry', 'registry.modRegistry');
+		$instance = $registry->getRegister('user', 'registry.modDbRegister');
+
+		$instance->connect();
+		$instance->subscribe('/sendex/subscribe/');
+		$instance->send('/sendex/subscribe/',
+			array(
+				$hash => array(
+					'user_id' => $user_id,
+					'newsletter_id' => $this->id,
+					'email' => $email,
+				)
+			),
+			array(
+				'ttl' => $linkTTL
+			)
+		);
+
+		return $hash;
+	}
+
+
+	/**
+	 * Confirms email of user
+	 *
+	 * @param $hash
+	 *
+	 * @return bool
+	 */
+	public function confirmEmail($hash) {
+		if (empty($hash)) {return false;}
+
+		/** @var modRegistry $registry */
+		$registry = $this->xpdo->getService('registry', 'registry.modRegistry');
+		$instance = $registry->getRegister('user', 'registry.modDbRegister');
+
+		$instance->connect();
+		$instance->subscribe('/sendex/subscribe/' . $hash);
+
+		$entry = $instance->read(array('poll_limit' => 1));
+		if (!empty($entry[0])) {
+			$entry = reset($entry);
+			if ($this->id != $entry['newsletter_id']) {
+				/** @var sxNewsletter $newsletter */
+				if ($newsletter = $this->xpdo->getObject('sxNewsletter', array('id' => $entry['newsletter_id'], 'active' => 1))) {
+					$newsletter->Subscribe($entry['user_id'], $entry['email']);
+				}
+				else {
+					return false;
+				}
+			}
+			else {
+				return $this->Subscribe($entry['user_id'], $entry['email']);
+			}
+		}
+
+		return false;
+	}
+
+
+	/**
+	 * Subscribes user to the newsletter
+	 *
+	 * @param int $user_id
+	 * @param string $email
+	 *
+	 * @return bool
+	 */
+	public function Subscribe($user_id = 0, $email = '') {
+		if (empty($email) && $profile = $this->xpdo->getObject('modUserProfile', array('internalKey' => $user_id))) {
+			$email = $profile->get('email');
+		}
+
+		if (empty($email) || !preg_match('/.+@.+\..+/i', $email)) {
+			return false;
+		}
+		elseif ($this->isSubscribed($user_id, $email)) {
+			return true;
+		}
+
+		$subscriber = $this->xpdo->newObject('sxSubscriber');
+		$subscriber->fromArray(array(
+			'newsletter_id' => $this->id,
+			'user_id' => $user_id,
+			'email' => $email
+		), '', true, true);
+
+		return $subscriber->save();
+	}
+
+
+	/**
+	 * Unsubscribes user from the newsletter
+	 *
+	 * @param string $code
+	 *
+	 * @return bool
+	 */
+	public function unSubscribe($code) {
+		if ($subscriber = $this->xpdo->getObject('sxSubscriber', array('code' => $code))) {
+			return $subscriber->remove();
+		}
+
+		return false;
 	}
 
 }
