efbe679327d42168f60cd83db84d4447cc409d98
Bezumkin <bezumkin@yandex.ru>
2013-12-12 16:07:52 +0700

    Auto generation of "code" on sxSubscriber create

--- core/components/sendex/model/sendex/sxsubscriber.class.php
+++ core/components/sendex/model/sendex/sxsubscriber.class.php
@@ -1,2 +1,11 @@
 <?php
-class sxSubscriber extends xPDOSimpleObject {}
+class sxSubscriber extends xPDOSimpleObject {
+
+	public function save($cacheFlag = null) {
+		$hash = sha1(uniqid(sha1($this->user_id . $this->newsletter_id . $this->email), true));
+
+		$this->set('code', $hash);
+		return parent::save($cacheFlag);
+	}
+
+}
