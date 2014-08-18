0ac9184f7df8db7850dd830fa9dba9bc793f4507
Bezumkin <bezumkin@yandex.ru>
2014-08-18 20:16:25 +0700

    - Compatibility with MODX 2.3. - UI improvements.

--- assets/components/sendex/js/mgr/misc/sendex.combo.js
+++ assets/components/sendex/js/mgr/misc/sendex.combo.js
@@ -9,21 +9,24 @@ Sendex.combo.User = function(config) {
 		,anchor: '99%'
 		,fields: ['username','id','fullname']
 		,pageSize: 20
-		,url: MODx.config.connectors_url + 'security/user.php'
+		,url: MODx.modx23
+			? MODx.config.connector_url
+			: MODx.config.connectors_url + 'security/user.php'
 		,editable: true
 		,allowBlank: true
 		,emptyText: _('sendex_select_user')
 		,baseParams: {
-			action: 'getlist'
+			action: MODx.modx23  ? 'security/user/getlist' : 'getlist'
 			,combo: 1
 		}
-		,tpl: new Ext.XTemplate(''
-			+'<tpl for="."><div class="sendex-list-item">'
-			+'<span><small>({id})</small> <b>{username}</b> ({fullname})</span>'
-			+'</div></tpl>',{
-			compiled: true
-		})
-		,itemSelector: 'div.sendex-list-item'
+		,tpl: new Ext.XTemplate(
+			'<tpl for=".">\
+				<div class="x-combo-list-item">\
+					<sup>({id})</sup> <strong>{username}</strong><br/>{fullname}\
+				</div>\
+			</tpl>'
+			,{compiled: true}
+		)
 	});
 	Sendex.combo.User.superclass.constructor.call(this,config);
 };
@@ -40,7 +43,7 @@ Sendex.combo.Newsletter = function(config) {
 		,displayField: 'name'
 		,valueField: 'id'
 		,anchor: '99%'
-		,fields: ['id','name']
+		,fields: ['id','name','description']
 		,pageSize: 20
 		,url: Sendex.config.connector_url
 		,editable: true
@@ -50,13 +53,14 @@ Sendex.combo.Newsletter = function(config) {
 			action: 'mgr/newsletter/getlist'
 			,combo: 1
 		}
-		,tpl: new Ext.XTemplate(''
-			+'<tpl for="."><div class="sendex-list-item">'
-			+'<span><small>({id})</small> {name}</span>'
-			+'</div></tpl>',{
-			compiled: true
-		})
-		,itemSelector: 'div.sendex-list-item'
+		,tpl: new Ext.XTemplate(
+			'<tpl for=".">\
+				<div class="x-combo-list-item">\
+					<sup>({id})</sup> <strong>{name}</strong><br/>{description}\
+				</div>\
+			</tpl>'
+			,{compiled: true}
+		)
 	});
 	Sendex.combo.Newsletter.superclass.constructor.call(this,config);
 };
