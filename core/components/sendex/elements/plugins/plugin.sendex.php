cef03cdd039c64982e034feea50b5f56e6ac66e3
Ruslan Aleev <aleev.insmile@gmail.com>
2019-08-19 19:45:29 +0400

    Corrected a tabs

--- core/components/sendex/elements/plugins/plugin.sendex.php
+++ core/components/sendex/elements/plugins/plugin.sendex.php
@@ -1,10 +1,9 @@
 <?php
-
 switch ($modx->event->name) {
 
-	case 'OnManagerPageInit':
-		$cssFile = MODX_ASSETS_URL.'components/sendex/css/mgr/main.css';
-		$modx->regClientCSS($cssFile);
-		break;
+    case 'OnManagerPageInit':
+        $cssFile = MODX_ASSETS_URL.'components/sendex/css/mgr/main.css';
+        $modx->regClientCSS($cssFile);
+        break;
 
 }
