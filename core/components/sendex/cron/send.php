793d32a097afecebc0c90a901458013fce039d83
Ivan Bochkarev <ivanx86@gmail.com>
2026-07-25 00:33:58 +0600

    fix(cron): Use dirname levels for config.core.php path
    Replace error-prone nested dirname() chain with dirname(__FILE__, 5).

--- core/components/sendex/cron/send.php
+++ core/components/sendex/cron/send.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once dirname(dirname(dirname(dirname(dirname(__FILE__))))) . '/config.core.php';
+require_once dirname(__FILE__, 5) . '/config.core.php';
 require_once MODX_CORE_PATH . 'config/' . MODX_CONFIG_KEY . '.inc.php';
 require_once MODX_CONNECTORS_PATH . 'index.php';
 require_once MODX_CORE_PATH . 'components/sendex/model/sendex/sxqueuesender.class.php';
