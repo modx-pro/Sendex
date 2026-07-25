3c9e55f0d75b253ede8ee1468cc6a21791be2d4b
Ivan Bochkarev <ivanx86@gmail.com>
2026-07-25 09:52:29 +0600

    fix(test): Load FakeModxServices stub in PHPUnit bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -17,6 +17,7 @@ require_once __DIR__ . '/Stubs/sxSubscriber.php';
 require_once __DIR__ . '/Stubs/sxQueue.php';
 require_once __DIR__ . '/Stubs/FakeMail.php';
 require_once __DIR__ . '/Stubs/FakeModX.php';
+require_once __DIR__ . '/Stubs/FakeModxServices.php';
 require_once __DIR__ . '/Stubs/FakePdoConnection.php';
 require_once __DIR__ . '/Stubs/FakePdoStatement.php';
 require_once __DIR__ . '/Stubs/modProcessor.php';
